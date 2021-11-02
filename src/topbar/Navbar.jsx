31bfca24f3c9c7e00f1e31fcd8cb0ba8a176a98f
geekySapien <escaping.blurred.reality@gmail.com>
2021-11-02 22:29:00 +0530

    Navbar component done

--- src/topbar/Navbar.jsx
+++ src/topbar/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Navbar.css';
-import { BsInstagram, BsTwitter, BsPinterest,BsSearch } from 'react-icons/bs';
+import { BsInstagram, BsTwitter, BsPinterest, BsSearch } from 'react-icons/bs';
 
 export default function Navbar() {
     return (
         <div className="h-12 sticky top-0 flex items-center top">
             <div className="left w-4/12">
                 <div className="flex justify-center gap-3">
-                    <BsInstagram className="w-6 h-6" />
-                    <BsTwitter className="w-6 h-6" />
-                    <BsPinterest className="w-6 h-6" />
+                    <BsInstagram className="w-6 h-6 text-instagramPink"  />
+                    <BsTwitter className="w-6 h-6 text-twitterBlue" />
+                    <BsPinterest className="w-6 h-6 text-pinterestRed" />
                 </div>
             </div>
             <div className="center w-8/12 ">
-                <div className="flex justify-center gap-3">
+                <div className="flex justify-center gap-3 text-gray-500 font-medium">
                     <div>HOME</div>
                     <div>ABOUT</div>
                     <div>CONTACT</div>
@@ -22,7 +22,16 @@ export default function Navbar() {
                 </div>
             </div>
             <div className="right w-4/12">
-                <BsSearch className="w-6 h-6"/>
+                <div className="flex items-center justify-center gap-3">
+                    <div className="w-12 h-12 ">
+                        <img
+                            src="https://images.unsplash.com/photo-1584626063607-b385c9f727cf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fHF1b3RlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                            alt=""
+                            className="w-full h-full rounded-full object-cover "
+                        />
+                    </div>
+                    <BsSearch className="w-6 h-6 text-gray-600 cursor-pointer" />
+                </div>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      twitterBlue: '#1D9BF0',
+      instagramPink: '#D32B7D',
+      pinterestRed: '#B7081B',
+    },
     extend: {},
   },
   variants: {
