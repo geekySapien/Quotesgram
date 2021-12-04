9725c6c6e3f884e35fd53b7fc23be098f431b738
geekySapien <escaping.blurred.reality@gmail.com>
2021-12-04 14:33:38 +0530

    fetched the route to delete a post using axios with handling data

--- client/src/components/SinglePost/SinglePostComponent.jsx
+++ client/src/components/SinglePost/SinglePostComponent.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import cover from '../../assets/quotesToRemember.png';
 import { MdEdit, MdDelete } from 'react-icons/md';
 import './SinglePostComponent.css';
 import axios from 'axios';
 import { useLocation } from 'react-router';
+import { Context } from './../../context/Context';
 export default function SinglePostComponent() {
     const location = useLocation();
     // console.log(location)
@@ -14,22 +15,39 @@ export default function SinglePostComponent() {
     useEffect(() => {
         const fetchPost = async () => {
             const res = await axios.get('/api/posts/' + path);
-            console.log(res);
+            //console.log(res);
             setPost(res.data.post);
-            console.log(post);
+            //console.log(post);
             //console.log(post.title);
         };
 
         fetchPost();
     }, [path]);
-const PF = 'http://localhost:5000/images/';
+    const PF = 'http://localhost:5000/images/';
+    const { user } = useContext(Context);
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.delete("/api/posts/" + path, {
+                data:{username: user.username}
+            });
+        //    setTimeout(() => {
+        //        console.log('deleted!');
+        //    }, 5000);
+            
+            window.location.replace("/");
+        } catch (err) {
+            console.log("Error deleting this post");
+        }
+    }
     return (
         <>
             <div className="mt-2 mr-6 ml-6 ">
                 {post.photo && (
                     <div className="w-full h-full  ">
                         <img
-                            src={PF+post.photo}
+                            src={PF + post.photo}
                             className="w-full h-full rounded-lg object-cover"
                         />
                     </div>
@@ -38,10 +56,12 @@ const PF = 'http://localhost:5000/images/';
                     <h1 className="text-2xl font-bold mt-4 postTitle ">
                         {post.title}
                     </h1>
-                    <div className="flex gap-3 icons ">
-                        <MdEdit className="mt-4 w-6 h-6 cursor-pointer text-green-600" />
-                        <MdDelete className="mt-4 w-6 h-6 cursor-pointer text-red-500" />
-                    </div>
+                    {post.username === user?.username && (
+                        <div className="flex gap-3 icons ">
+                            <MdEdit className="mt-4 w-6 h-6 cursor-pointer text-green-600" />
+                            <MdDelete className="mt-4 w-6 h-6 cursor-pointer text-red-500" onClick={handleDelete}/>
+                        </div>
+                    )}
                 </div>
                 <div className="flex justify-between mt-3">
                     <p className="text-lg">
--- server/routes/users.js
+++ server/routes/users.js
@@ -15,7 +15,7 @@ router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
-      req.body.passowrd = await bcrypt.hash(req.body.password, salt);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
       const updatedUser = await User.findByIdAndUpdate(
